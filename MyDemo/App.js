923308df3201cd8fd17fd3d8f6a46c407faede8c
Ashok Parthiban D <aparthiban@wisilica.com>
2018-02-17 17:03:32 +0530

    Scroll View Update

--- MyDemo/App.js
+++ MyDemo/App.js
@@ -9,8 +9,12 @@ import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,ScrollView
 } from 'react-native';
+
+// import styles from './Styles/styles'
+
+var styles = require('./Styles/styles')
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
@@ -24,6 +28,47 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
+        <View style = {{alignItems:'center',justifyContent:'center',height:60,backgroundColor:'#edf1f2'}}>
+          <Text style = {{fontSize:40,fontWeight:"bold",color:'#386268',textShadowRadius:10}}>My Home</Text>  
+        </View>
+        <View style = {{alignItems:'center',justifyContent:'center',flex:2}}>
+        <ScrollView >
+          <Text style={{fontSize:50}}>Scroll me plz</Text>
+          <View style= {{backgroundColor:"powderblue",height:60}}/>
+          <View style= {{backgroundColor:"skyblue",height:60}}/>
+          <View style= {{backgroundColor:"steelblue",height:60}}/>
+
+          <Text style={{fontSize:50}}>If you like</Text>
+          <View style= {{backgroundColor:"powderblue",height:60}}/>
+          <View style= {{backgroundColor:"skyblue",height:60}}/>
+          <View style= {{backgroundColor:"steelblue",height:60}}/>
+
+          <Text style={{fontSize:50}}>Scrolling down</Text>
+          <View style= {{backgroundColor:"powderblue",height:60}}/>
+          <View style= {{backgroundColor:"skyblue",height:60}}/>
+          <View style= {{backgroundColor:"steelblue",height:60}}/>
+
+          <Text style={{fontSize:50}}>What's the best</Text>
+          <View style= {{backgroundColor:"powderblue",height:60}}/>
+          <View style= {{backgroundColor:"skyblue",height:60}}/>
+          <View style= {{backgroundColor:"steelblue",height:60}}/>
+
+          <Text style={{fontSize:50}}>Framework around?</Text>
+          <View style= {{backgroundColor:"powderblue",height:60}}/>
+          <View style= {{backgroundColor:"skyblue",height:60}}/>
+          <View style= {{backgroundColor:"steelblue",height:60}}/>
+          <Text style={{fontSize:80}}>React Native</Text>
+        </ScrollView>
+        </View>
+      </View>
+    );
+  }
+}
+
+
+
+
+{/* <View style={styles.container}>
         <Text style={styles.welcome}>
           Welcome to React Native!
         </Text>
@@ -33,26 +78,4 @@ export default class App extends Component<Props> {
         <Text style={styles.instructions}>
           {instructions}
         </Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+      </View> */}
---
+++ MyDemo/Styles/styles.js
@@ -0,0 +1,25 @@
+import {StyleSheet} from 'react-native';
+
+module.exports = StyleSheet.create({
+    container: {
+      flex: 1,
+      // justifyContent: 'center',
+      // alignItems: 'center',
+      // backgroundColor: '#F45420',
+      flexDirection:'column'
+      
+    },
+    welcome: {
+      fontSize: 20,
+      textAlign: 'center',
+      margin: 10,
+      color: '#FFFFFF',
+    },
+    instructions: {
+      textAlign: 'center',
+      color: '#FFFFFF',
+      marginBottom: 5,
+    },
+  });
+
+  
