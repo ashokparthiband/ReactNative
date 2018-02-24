10d8695c9d664088e5f74392f60bf22f16c4b95e
Ashok Parthiban D <aparthiban@wisilica.com>
2018-02-24 22:32:43 +0530

    Devices Scan Page Design

--- ReactWithBLE/App.js
+++ ReactWithBLE/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import CustomListView from './App/CustomListView';
+
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <View style={styles.titleView}><Text style={styles.text}>Devices</Text></View>
+        <CustomListView></CustomListView>
       </View>
     );
   }
@@ -15,9 +16,16 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: '#eff0f2',
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'column',
   },
+  text:{
+    fontSize: 30,
+    textAlign: 'center'
+  },
+  titleView:{
+    marginTop: 30,
+    backgroundColor:'#eff0f2'
+  }
 });
