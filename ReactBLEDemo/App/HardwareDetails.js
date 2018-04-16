f49bb487718fa0d390c41a29fdccacd00962ccdb
A Parthiban <ashokparthiband@gmail.com>
2018-04-16 22:42:18 +0530

    What to do

--- ReactBLEDemo/App/HardwareDetails.js
+++ ReactBLEDemo/App/HardwareDetails.js
@@ -1,5 +1,11 @@
 import React from 'react';
-import { View, ListView, StyleSheet, Text,TouchableHighlight } from 'react-native';
+import { View, ListView, StyleSheet, Text,TouchableHighlight, ScrollView } from 'react-native';
+
+import {
+  Cell,
+  Section,
+  TableView,
+} from 'react-native-tableview-simple';
 
 
 class HardwareDetails extends React.Component {
@@ -7,10 +13,60 @@ class HardwareDetails extends React.Component {
       super(props);
     }
 
+    static navigationOptions = ({navigation}) => ({
+      title: "Device Info",
+      headerTitleStyle:{
+        fontSize:24,
+        alignSelf:'center'
+      },
+    });
+
     render() {
       return (
-        <View>
-            
+        <View style={styles.container}>
+           <ScrollView style={styles.stage}>
+            <TableView>
+              <Section header="Basic Info">
+                <Cell
+                  cellStyle="Subtitle"
+                  title="Name"
+                  detail="Heart Rate Moniter"
+                />
+                <Cell
+                  cellStyle="Subtitle"
+                  title="Hardware Version"
+                  detail="20.25.30"
+                />
+                <Cell
+                  cellStyle="Subtitle"
+                  title="Software Version"
+                  detail="0.0.8"
+                />
+                <Cell
+                  cellStyle="Subtitle"
+                  title="Firmware Version"
+                  detail="5.9.8"
+                />
+            </Section>
+            <Section header="Heart Rate Info">
+                <Cell
+                  cellStyle="Subtitle"
+                  title="Sensor Location"
+                  detail="Chest"
+                />
+                <Cell
+                  cellStyle="Subtitle"
+                  title="Heart Point"
+                  // detail="0.0.8"
+                />
+                <Cell
+                  cellStyle="Subtitle"
+                  title="Heart Rate Mesurement"
+                  detail="102"
+                />
+            </Section>
+            </TableView> 
+           </ScrollView>
         </View>
       );
     }
@@ -20,7 +76,10 @@ const styles = StyleSheet.create({
     container: {
       flex: 1,
       backgroundColor:'white'
-    }
+    },
+    stage: {
+      backgroundColor: '#EFEFF4',
+    },
 });
 
 export default HardwareDetails;
